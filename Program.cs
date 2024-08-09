9f1df10ec5b14abfcd1469126db9a05538ab36dc
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2024-08-08 22:50:59 -0600

    Ya pido los datos generales que tiene cada uno de los tipos de habitación. Realicé cambios mínimos en el switch principal.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class program
 {
     static void Main(string[] args)
     {
-        int opcion = 0;
+        int opcion = 0, opcionHabitacion = 0;
         List<Habitacion> listaHabitaciones = new List<Habitacion>();
         Habitacion nuevaHabitacion = new Habitacion(0, true, "");
         do
@@ -27,18 +27,66 @@ class program
                 switch (opcion)
                 {
                     case 1:
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine("     AGREGAR HABITACION");
-                        Console.WriteLine("----------------------------");
-                        Console.ResetColor(); Console.WriteLine();
-                        Console.WriteLine("¿Qué tipo de habitación desea agregar?");
-                        Console.WriteLine("1. Habitación Simple");
-                        Console.WriteLine("2. Habitación Doble");
-                        Console.WriteLine("3. Suite");
-                        Console.WriteLine("4. Habitación Deluxe");
-                        nuevaHabitacion.MensajeContinuar();
+                        do
+                        {
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine("----------------------------");
+                            Console.WriteLine("     AGREGAR HABITACION");
+                            Console.WriteLine("----------------------------");
+                            Console.ResetColor(); Console.WriteLine();
+                            Console.WriteLine("¿Qué tipo de habitación desea agregar?");
+                            Console.WriteLine("1. Habitación Simple");
+                            Console.WriteLine("2. Habitación Doble");
+                            Console.WriteLine("3. Suite");
+                            Console.WriteLine("4. Habitación Deluxe");
+                            Console.WriteLine("5. Ninguna");
+                            Console.WriteLine(); Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("Ingresa el no. de opción: "); Console.ResetColor();
+                            try
+                            {
+                                opcionHabitacion = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Ingresa el precio por noche: ");
+                                double precioPorNoche = Convert.ToDouble(Console.ReadLine());
+                                Console.Write("¿Está disponible? (true/false): ");
+                                bool disponibilidad = Convert.ToBoolean(Console.ReadLine());
+                                string nombreCliente = "";
+                                if (disponibilidad = false) 
+                                {
+                                    Console.Write("Ingresa el nombre del cliente: ");
+                                    nombreCliente = Console.ReadLine();
+                                }
+                                switch (opcionHabitacion)
+                                {
+                                    case 1:
+                                        Console.Write("Ingresa el número de camas: ");
+                                        int numeroDeCamas = Convert.ToInt32(Console.ReadLine());
+                                        HabitacionSimple nuevaHabitacionSimple = new HabitacionSimple(precioPorNoche, disponibilidad, nombreCliente, numeroDeCamas);
+                                        listaHabitaciones.Add(nuevaHabitacionSimple);
+                                        nuevaHabitacion.MensajeContinuar();
+                                        break;
+                                    case 2:
+                                        Console.Write("Ingresa ")
+                                        nuevaHabitacion.MensajeContinuar();
+                                        break;
+                                    case 3:
+                                        nuevaHabitacion.MensajeContinuar();
+                                        break;
+                                    case 4:
+                                        nuevaHabitacion.MensajeContinuar();
+                                        break;
+                                    default:
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine("Esta opcion no existe. Intenta de nuevo...");
+                                        nuevaHabitacion.MensajeContinuar();
+                                        break;
+                                }
+                            }catch(FormatException)
+                            {
+                                nuevaHabitacion.MensajeError();
+                                nuevaHabitacion.MensajeContinuar();
+                            }
+                        } while (opcionHabitacion != 5);
                         break;
                     case 2:
                         Console.Clear();
@@ -70,11 +118,10 @@ class program
                     case 5:
                         Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine("     Ten un buen día.");
-                        Console.WriteLine("----------------------------");
-                        Console.ResetColor(); Console.WriteLine();
-                        nuevaHabitacion.MensajeContinuar();
+                        Console.WriteLine("-----------------------------");
+                        Console.WriteLine("     Ten un buen día :)");
+                        Console.WriteLine("-----------------------------");
+                        Console.ResetColor(); 
                         break;
                     default:
                         Console.ForegroundColor= ConsoleColor.Red;
